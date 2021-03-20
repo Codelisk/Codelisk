515fab7b51aa46e8107a3bb1460ba94eadfbeb96
d.hufnagl <d.hufnagl@codelisk.com>
2021-03-20 17:05:28 +0100

    Git init

---
+++ Codelisk.Base/BaseModule.cs
@@ -0,0 +1,20 @@
+﻿using Codelisk.Base.ViewModels;
+using Codelisk.Base.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+
+namespace Codelisk.Base
+{
+    public class BaseModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<ViewA, ViewAViewModel>();
+        }
+    }
+}
